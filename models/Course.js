b68107eed5de520fc2bd8058c5f2f343b1bc53d0
Abhishek Singh <abhishek.singh.951923@gmail.com>
2026-01-31 04:10:33 +0530

    Initial

---
+++ models/Course.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const courseSchema = new mongoose.Schema({
+      courseName: {
+            type: String,
+            trim: true,
+            required: true,
+      },
+      courseDescription: {
+            type:String,
+            ref:"User",
+            required:true,
+      },
+      instructor: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"User",
+            required:true,
+      },
+      whatYouWillLearn: {
+            type: String,
+      },
+      courseContent: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Section",
+      },
+      ratingAndReviews: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "RatingAndReviews",
+      }],
+      price: {
+            type: Number,
+      },
+      thumbnail: {
+            type: String,
+      },
+      tag: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"Tags",
+      },
+      studentsEnrolled: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"User"
+      }
+});
+module.exports = mongoose.model("Course", courseSchema);
